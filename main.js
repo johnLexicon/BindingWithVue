58a04f45e75a94bbcc986b59a19a6750df8326f2
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-11-15 14:28:03 +0100

    Form elements binding with v-model.

---
+++ main.js
@@ -0,0 +1,13 @@
+var formView = new Vue({
+  el: "#form",
+  data: {
+    appName: "FormView",
+    query: "",
+    firstName: "",
+    age: 50,
+    comments: "",
+    gender: "male",
+    agreement: false,
+    berries: ["blueberries", "blackberries"],
+  },
+});
